9cf679b84fcd09db309c38020c105b8f07da40fc
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    r5x: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b

--- device.mk
+++ device.mk
@@ -31,8 +31,7 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     libbatterylistener \
