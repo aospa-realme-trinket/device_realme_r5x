1440203adbb7482b88ffa5c0b9a1ea4663b2ff25
Dakkshesh <dakkshesh5@gmail.com>
2024-04-11 23:07:16 +0600

    r5x: Drop power stats service
    07-29 19:30:18.343   991   991 E android.hardware.power.stats@1.0-service-mock: Failed to getEnergyData

--- device.mk
+++ device.mk
@@ -347,8 +347,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Perf
 PRODUCT_PACKAGES += \
