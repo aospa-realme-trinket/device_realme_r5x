9ae21f988bdc37ae5f5d92565a12f728d9e9e367
mikeNG <mikeNG@users.noreply.github.com>
2024-11-03 19:03:54 +0000

    r5x: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -319,7 +319,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mock
+    android.hardware.thermal-service.qti
 
 # Update
 AB_OTA_UPDATER := false
