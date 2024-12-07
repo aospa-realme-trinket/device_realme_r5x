46a7992793c8701dd3a548f7accce7ae11f42321
ZVNexus <ZVNexus@outlook.com>
2024-12-07 21:59:10 +0600

    r5x: Drop PRODUCT_BUILD_SUPER_PARTITION
     * Super image is not generated by a default 'm' build,
       but we at least want it for dist builds.
    
    Change-Id: Ia12a5314632509fcd309d51ddf757efdc37499c3

--- device.mk
+++ device.mk
@@ -126,7 +126,6 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 # Dynamic Partitions
-PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
